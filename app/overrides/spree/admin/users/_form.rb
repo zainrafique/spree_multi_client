f4af165591ba4c16a437f3a469e2a7d54f76d4ef
zainrafique <zainraf21@gmail.com>
2020-02-20 16:16:37 +0500

    client base roles

--- app/overrides/spree/admin/users/_form.rb
+++ app/overrides/spree/admin/users/_form.rb
@@ -1,18 +1,6 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/users/_form',
-  name: 'Add vendor select in user form',
-  insert_bottom: 'div[data-hook="admin_user_form_roles"]',
-  text: <<-HTML
-          <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
-            <%= f.field_container :vendor_ids, class: ['form-group'] do %>
-              <%= f.label :vendor_ids, Spree.t(plural_resource_name(Spree::Vendor)) %>
-              <%= f.collection_select(:vendor_ids, Spree::Vendor.all, :id, :name, { }, { class: 'select2', multiple: true }) %>
-            <% end %>
-          <% elsif defined?(current_spree_client) && current_spree_client %>
-            <%= f.field_container :vendor_ids, class: ['form-group'] do %>
-              <%= f.label :vendor_ids, Spree.t(plural_resource_name(Spree::Vendor)) %>
-              <%= f.collection_select(:vendor_ids, current_spree_client.vendors, :id, :name, { }, { class: 'select2', multiple: true }) %>
-            <% end %>
-          <% end %>
-        HTML
-)
+    virtual_path: 'spree/admin/users/_form',
+    name: 'show_roles_according_to_client',
+    replace: 'div[data-hook="admin_user_form_roles"]',
+    partial: "spree/admin/users/client_roles",
+    )
