857a76e68c5ea3a19fec780b2396239554274135
zainrafique <zainraf21@gmail.com>
2020-03-03 12:17:50 +0500

    add vendor names

--- app/controllers/spree/admin/states_controller_decorator.rb
+++ app/controllers/spree/admin/states_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::Admin::StatesController.class_eval do
   
   protected
   def load_data
-    @countries = (defined?(current_spree_client) && current_spree_client) ? current_spree_client.shipping_methods : Country.order(:name)
+    @countries = (defined?(current_spree_client) && current_spree_client) ? current_spree_client.countries : Country.order(:name)
   end
 
 end
