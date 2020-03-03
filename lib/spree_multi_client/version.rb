dbe28221c000946410722690f01bd542d808180b
zainrafique <zainraf21@gmail.com>
2020-03-03 12:18:21 +0500

    updated version number

--- lib/spree_multi_client/version.rb
+++ lib/spree_multi_client/version.rb
@@ -10,7 +10,7 @@ module SpreeMultiClient
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 8
+    TINY  = 9
     PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
