12b604c22495405ce90234b52962816fe7bbd3d2
zainrafique <zainraf21@gmail.com>
2020-03-04 18:55:37 +0500

    fix relation type issue

--- app/models/spree/relation_type_decorator.rb
+++ app/models/spree/relation_type_decorator.rb
@@ -1,8 +1,6 @@
-Spree::Vendor.class_eval do
+Spree::RelationType.class_eval do
   clear_validators!
 
   validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :client_id }
-
-  validates :slug, uniqueness: true
-  
+	
 end
