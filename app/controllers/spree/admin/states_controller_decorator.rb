1f582d0333c2da39744966a623d9773e791ed73b
zainrafique <zainraf21@gmail.com>
2020-05-11 13:11:52 +0500

    fix country and zone issue

--- app/controllers/spree/admin/states_controller_decorator.rb
+++ app/controllers/spree/admin/states_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::Admin::StatesController.class_eval do
   protected
   def load_data
     # @countries = (defined?(current_spree_client) && current_spree_client) ? current_spree_client.countries : Country.order(:name)
-    @countries = Country.order(:name)
+    @countries = Spree::Country.order(:name)
   end
 
 end
--- app/controllers/spree/admin/zones_controller_decorator.rb
+++ app/controllers/spree/admin/zones_controller_decorator.rb
@@ -4,9 +4,9 @@ Spree::Admin::ZonesController.class_eval do
   protected
 	 
   def load_data
-    @countries = Country.order(:name)
-    @states = State.order(:name)
-    @zones = (defined?(current_spree_client) && current_spree_client) ? current_spree_client.zones.order(:name) :  Zone.order(:name)
+    @countries = Spree::Country.order(:name)
+    @states = Spree::State.order(:name)
+    @zones = (defined?(current_spree_client) && current_spree_client) ? current_spree_client.zones.order(:name) :  Spree::Zone.order(:name)
   end
 
 end
